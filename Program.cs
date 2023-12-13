3fbc2d4551abc573b4beb9067a4f427b7ca1aa41
OMFrydenlund <migfrydenlund@gmail.com>
2023-12-13 11:08:20 +0100

    Choice functionality added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using ifElse;
 
 //complete final task
@@ -8,7 +9,44 @@ class Program
 {
     static void Main()
     {
-        //IfElse.EqualChecker();
-        AddNumbers.AddRun();
+        ProgRun();
+    }
+
+    private static void ProgRun()
+    {
+        Console.Clear();
+        Console.WriteLine("1.Add");
+        Console.WriteLine("2.Check equal value");
+        Console.WriteLine("3.Check true or false");
+        Console.WriteLine(" (press 1 - 3)");
+        var userInput = ParseInput(3, "Can't do that!");
+        switch (userInput)
+        {
+            case 1:
+                AddNumbers.AddRun();
+                break;
+            case 2:
+                IfElse.EqualChecker();
+                break;
+            case 3:
+                TrueFalse.BoolChecker();
+                break;
+        }
+
+        static int ParseInput(int max, string errorMessage = "Number out of range!")
+        {
+            var input = Console.ReadLine();
+            try
+            {
+                var number = int.Parse(input);
+                if (number >= 0 && number <= max) return number;
+                Console.WriteLine(errorMessage);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Not a number, try again.");
+            }
+            return ParseInput(max);
+        }
     }
 }
---
+++ TrueFalse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifElse
+{
+    internal class TrueFalse
+    {
+        public TrueFalse(int a, int b)
+        {
+            FirstNumber = a;
+            SecondNumber = b;
+            int sum = a + b;
+        }
+
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public static void BoolChecker()
+        {
+            TrueFalse instance = new TrueFalse(15, 32);
+            bool result = TrueFalseResponse(instance.FirstNumber, instance.SecondNumber);
+            Console.WriteLine(result);
+        }
+
+        public static bool TrueFalseResponse(int a, int b)
+        {
+            int sum = a + b;
+
+            if (sum  == 30)
+                return true;
+            else
+                return false;
+        }
+    }
+}
